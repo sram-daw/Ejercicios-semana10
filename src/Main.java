d4c74529b46d8679ce6deb50f1d5fd5c85bc5952
sram-daw <sramiloalonso@danielcastelao.org>
2022-12-04 21:08:15 +0100

    CAMBIO BUCLE: simplificacion

--- src/Main.java
+++ src/Main.java
@@ -11,104 +11,90 @@ public class Main {
         /*Por cada ejecución del programa instanciamos un nuevo objeto parking.
         Ese constructor se encarga de crear el objeto parking con los atributos que hemos especificado: la capacidad indicada por el usuario y
         la posición actual del array, que en este momento es 0 ya que aun no hemos empezado el bucle for del menú que va a ir aumentando este valor.*/
-        Parking parking = new Parking(capacidadParking, 0);
-        while (!salir) { //Son necesarios ambos bucles (while y for) porque si se añade la condición !salir solo en el for, se para la ejecución cuando se retira un vehículo al no cumplirse la condición de isAparcar.
+        Parking parking = new Parking(capacidadParking/*, 0*/);
+        while (!salir) { //Para la ejecución del programa cuando el usuario elige salir.
             //Bucle menú
             int operacion = 0;
-            boolean isAparcar = true; /*este booleano nos permite añadir una condición más en el for: solo contará iteraciones en el bucle si es true,
-             para que si retiramos un vehículo (o no se puede aparcar por falta de espacio) no se desplace un lugar en el array.*/
-            for (int i = 0; i < parking.plazas.length && isAparcar && !salir; i++) {
-                System.out.println("¿Qué desea hacer? \n1. Aparcar vehículo \n2. Retirar vehículo. \n3. Mostrar vehículos aparcados y plazas disponibles. \n4. Localizar un vehículo.  \n5. Salir.");
-                operacion = entrada.nextInt();
-                switch (operacion) {
+            System.out.println("¿Qué desea hacer? \n1. Aparcar vehículo \n2. Retirar vehículo. \n3. Mostrar vehículos aparcados y plazas disponibles. \n4. Localizar un vehículo.  \n5. Salir.");
+            operacion = entrada.nextInt();
+            switch (operacion) {
 
-                    case 1:
-                        if (parking.comprobarPlazasLibres() >= 1) {
-                            int modoIntroduccion;
-                            Scanner entradaModoAparca = new Scanner(System.in);
-                            System.out.println("¿Cómo desea aparcar el vehículo? \n1. Automáticamente  \n2. Manualmente ");
-                            modoIntroduccion = entradaModoAparca.nextInt();
-                            //Aparcamiento manual
-                            if (modoIntroduccion == 2) {
-                                vehiculoNuevo = Vehiculo.crearVehiculosManual();
-                                System.out.println(vehiculoNuevo.toString()); //Para los test: muestra los atributos del vehículo nuevo haciendo uso del método generado "toString".
-                                if (vehiculoNuevo.getSize() == 1) { //Si es un coche, llamamos al método aparcar.
-                                    parking.aparcarVehiculo(vehiculoNuevo, i);//se llama al objeto parking, no a la clase, porque en este punto del programa ya es un objeto con unos atributos determinados.
-                                    parking.setPosicionParking(i); //Hacemos que posicionParking (posición actual del índice) vaya a la par que la i del bucle.
-                                }
-                                if (vehiculoNuevo.getSize() == 2 && parking.comprobarDosPlazasConsecutivasLibres()) { //Si es un camión y hay 2 plazas libres consecutivas, llamamos al método aparcar y...
-                                    parking.aparcarVehiculo(vehiculoNuevo, i);
-                                    i += 1; //... debemos añadir 1 posición extra a la posición del array, porque ocupa 2 huecos.
-                                    parking.setPosicionParking(i);
-                                }
-                                if (vehiculoNuevo.getSize() == 2 && parking.comprobarDosPlazasConsecutivasLibres() == false) {
-                                    isAparcar = false;
-                                    System.out.println("No hay dos plazas libres consecutivas para aparcar el camión.");
-                                }
+                case 1:
+                    if (parking.comprobarPlazasLibres() >= 1) {
+                        int modoIntroduccion;
+                        Scanner entradaModoAparca = new Scanner(System.in);
+                        System.out.println("¿Cómo desea aparcar el vehículo? \n1. Automáticamente  \n2. Manualmente ");
+                        modoIntroduccion = entradaModoAparca.nextInt();
+                        //Aparcamiento manual
+                        if (modoIntroduccion == 2) {
+                            vehiculoNuevo = Vehiculo.crearVehiculosManual();
+                            System.out.println("Datos introducidos: \n" + vehiculoNuevo.toString()); //Para los test: muestra los atributos del vehículo nuevo haciendo uso del método generado "toString".
+                            if (vehiculoNuevo.getSize() == 1) { //Si es un coche, llamamos al método aparcar.
+                                parking.aparcarVehiculo(vehiculoNuevo);//se llama al objeto parking, no a la clase, porque en este punto del programa ya es un objeto con unos atributos determinados.
                             }
-                            //Aparcamiento automatico
-                            if (modoIntroduccion == 1) {
-                                vehiculoNuevo = new Vehiculo(); //se genera un nuevo objeto Vehiculo usando el constructor parametrizado que lo genera automáticamente.
-                                System.out.println(vehiculoNuevo.toString());//Para los test: muestra los atributos del vehículo nuevo haciendo uso del método generado "toString".
-                                if (vehiculoNuevo.getSize() == 1) {
-                                    parking.aparcarVehiculo(vehiculoNuevo, i); //se llama al objeto parking, no a la clase, porque en este punto del programa ya es un objeto con unos atributos determinados (array plazas[] y n_plazas)
-                                    parking.setPosicionParking(i);
-                                }
-                                if (vehiculoNuevo.getSize() == 2 && parking.comprobarDosPlazasConsecutivasLibres()) {
-                                    parking.aparcarVehiculo(vehiculoNuevo, i);
-                                    i += 1;
-                                    parking.setPosicionParking(i);
-                                }
-                                if (vehiculoNuevo.getSize() == 2 && parking.comprobarDosPlazasConsecutivasLibres() == false) {
-                                    isAparcar = false;
-                                    System.out.println("No hay dos plazas libres consecutivas para aparcar el camión.");
-                                }
+                            if (vehiculoNuevo.getSize() == 2 && parking.comprobarDosPlazasConsecutivasLibres()) { //Si es un camión y hay 2 plazas libres consecutivas, llamamos al método aparcar y...
+                                parking.aparcarVehiculo(vehiculoNuevo);
                             }
-                            System.out.println(parking.toString()); // Para los test: muestra el array del parking.
-
-                        } else {
-                            isAparcar = false;
-                            System.out.println("No hay plazas disponibles.");
+                            if (vehiculoNuevo.getSize() == 2 && parking.comprobarDosPlazasConsecutivasLibres() == false) {
+                                System.out.println("No hay dos plazas libres consecutivas para aparcar el camión.");
+                            }
                         }
-                        break;
-                    case 2:
-                        isAparcar = false;
-                        String matriculaVehiculoDesaparcar = "";
-                        Scanner entradaDesaparcar = new Scanner(System.in);
-                        System.out.print("Indique la matrícula correspondiente al vehículo que desea retirar: ");
-                        matriculaVehiculoDesaparcar = entradaDesaparcar.nextLine();
-                        parking.desaparcarVehiculo(matriculaVehiculoDesaparcar);
-                        System.out.println("Vehículo retirado correctamente.");
-                        break;
-
-                    case 3:
-                        System.out.println(parking.toString()); //Función para visualizar el contenido del parking.
-                        break;
-
-                    case 4:
-                        isAparcar = false;
-                        int metodoLocalizarVehiculo = 0;
-                        Scanner entradaLocalizar = new Scanner(System.in);
-                        System.out.println("Desea localizar el vehículo: \n1. Por plaza \n2. Por matrícula");
-                        metodoLocalizarVehiculo = entradaLocalizar.nextInt();
-                        if (metodoLocalizarVehiculo == 1) {
-                            int plazaIntroducida = 0;
-                            System.out.print("Introduzca el número de plaza (posición): ");
-                            plazaIntroducida = entradaLocalizar.nextInt();
-                            parking.obtenerVehiculoPorPlaza(plazaIntroducida);
+                        //Aparcamiento automatico
+                        if (modoIntroduccion == 1) {
+                            vehiculoNuevo = new Vehiculo(); //se genera un nuevo objeto Vehiculo usando el constructor parametrizado que lo genera automáticamente.
+                            System.out.println("Datos introducidos: \n" + vehiculoNuevo.toString());//Para los test: muestra los atributos del vehículo nuevo haciendo uso del método generado "toString".
+                            if (vehiculoNuevo.getSize() == 1) {
+                                parking.aparcarVehiculo(vehiculoNuevo);
+                            }
+                            if (vehiculoNuevo.getSize() == 2 && parking.comprobarDosPlazasConsecutivasLibres()) {
+                                parking.aparcarVehiculo(vehiculoNuevo);
+                            }
+                            if (vehiculoNuevo.getSize() == 2 && parking.comprobarDosPlazasConsecutivasLibres() == false) {
+                                System.out.println("No hay dos plazas libres consecutivas para aparcar el camión.");
+                            }
                         }
-                        if (metodoLocalizarVehiculo == 2) {
-                            String matriculaIntroducida = "";
-                            Scanner entradaMatriculaLocalizar = new Scanner(System.in);
-                            System.out.print("Introduzca la matrícula del vehículo a localizar: ");
-                            matriculaIntroducida = entradaMatriculaLocalizar.nextLine();
-                            parking.obtenerVehiculoPorMatricula(matriculaIntroducida);
-                        }
-                        break;
-                    case 5:
-                        salir = true;
-                        break;
-                }
+                        /* System.out.println(parking.toString()); // Para los test: muestra el array del parking.*/
+
+                    } else {
+                        System.out.println("No hay plazas disponibles.");
+                    }
+                    break;
+                //Retirar un vehículo
+                case 2:
+                    String matriculaVehiculoDesaparcar = "";
+                    Scanner entradaDesaparcar = new Scanner(System.in);
+                    System.out.print("Indique la matrícula correspondiente al vehículo que desea retirar: ");
+                    matriculaVehiculoDesaparcar = entradaDesaparcar.nextLine();
+                    parking.desaparcarVehiculo(matriculaVehiculoDesaparcar);
+                    System.out.println("Vehículo retirado correctamente.");
+                    break;
+
+                case 3:
+                    System.out.println(parking.toString()); //Función para visualizar el contenido del parking.
+                    break;
+                //Localizar vehículos
+                case 4:
+                    int metodoLocalizarVehiculo = 0;
+                    Scanner entradaLocalizar = new Scanner(System.in);
+                    System.out.println("Desea localizar el vehículo: \n1. Por plaza \n2. Por matrícula");
+                    metodoLocalizarVehiculo = entradaLocalizar.nextInt();
+                    if (metodoLocalizarVehiculo == 1) {
+                        int plazaIntroducida = 0;
+                        System.out.print("Introduzca el número de plaza (posición): ");
+                        plazaIntroducida = entradaLocalizar.nextInt();
+                        parking.obtenerVehiculoPorPlaza(plazaIntroducida);
+                    }
+                    if (metodoLocalizarVehiculo == 2) {
+                        String matriculaIntroducida = "";
+                        Scanner entradaMatriculaLocalizar = new Scanner(System.in);
+                        System.out.print("Introduzca la matrícula del vehículo a localizar: ");
+                        matriculaIntroducida = entradaMatriculaLocalizar.nextLine();
+                        parking.obtenerVehiculoPorMatricula(matriculaIntroducida);
+                    }
+                    break;
+                case 5:
+                    salir = true;
+                    break;
             }
         }
     }
--- src/Parking.java
+++ src/Parking.java
@@ -6,13 +6,12 @@ public class Parking {
 
     //n_plazas viene dado por el número que indica el usuario para la capacidad del parking.
     int n_plazas;
-    int posicionParking; //Es la posición actual del array. Depende del bucle for del main.
+
 
     //En este constructor parametrizado del objeto parking estamos indicando que el atributo plazas va a ser igual a un array con un número de índices determinado por n_plazas.
-    public Parking(int n_plazas, int posicionParking) {
+    public Parking(int n_plazas) {
         this.n_plazas = n_plazas;
         this.plazas = new Vehiculo[this.n_plazas];
-        this.posicionParking = posicionParking;
     }
 
 
@@ -32,64 +31,36 @@ public class Parking {
         this.plazas = plazas;
     }
 
-    public int getPosicionParking() {
-        return posicionParking;
-    }
-
-    public void setPosicionParking(int posicionParking) {
-        this.posicionParking = posicionParking;
-    }
-
     @Override
     public String toString() {
         return "Plazas totales: " + n_plazas + "\n" +
                 "Plazas vacías: " + comprobarPlazasLibres() + "\n" +
                 "Vehículos aparcados: " + "\n" +
-                Arrays.toString(plazas) +
-                "TEST posicionParking: " + posicionParking;
+                Arrays.toString(plazas);
     }
 
     //Introduce los objetos vehículo en el array
-    public void aparcarVehiculo(Vehiculo vehiculoNuevo, int posicionParking) { /*pasamos por parámetros el nuevo objeto vehículo creado y la posición
-         del array del bucle del main para así saber en qué posición debe la función colocar el objeto.*/
-
-        //Esta primera parte comprueba si la plaza actual en el array está libre.
-        if (this.plazas[posicionParking] == null) { //Si la posición del array correspondiente a la iteración está vacía...
-            if (vehiculoNuevo.getSize() == 1) {
-                this.plazas[posicionParking] = vehiculoNuevo; //... aparcar el vehículo si tiene size=1 (si es un coche). Y...
-                System.out.println("Vehículo aparcado correctamente.");
-            }
-            if (vehiculoNuevo.getSize() == 2 ) { //... si el vehículo tiene size=2 (es un camión)...
-                this.plazas[posicionParking] = vehiculoNuevo;
-                this.plazas[posicionParking + 1] = vehiculoNuevo;//... aparcar el camión en ambas plazas (en i y en i+1).
-                System.out.println("Vehículo aparcado correctamente.");
-
-            }
-            //A partir de aquí, si la siguiente plaza no está libre comprueba todas las demás.
-        } else {
-            if (vehiculoNuevo.getSize() == 1) { //Este if es para los coches
-                boolean pararCuandoNull= false; //Este booleano es necesario para evitar que, tras retirar un vehículo y deje de seguirse el orden del índice, se aparque en todas las posiciones null.
-                for (int i = 0; i < plazas.length && !pararCuandoNull; i++) {
-                    if (this.plazas[i] == null) {
-                        pararCuandoNull=true;
-                        this.plazas[i] = vehiculoNuevo;
-                        System.out.println("Vehículo aparcado correctamente.");
-                    }
-                }
-            }
-            if (vehiculoNuevo.getSize() == 2) { //Este if es para los camiones.
-                boolean pararCuandoNull= false;
-                for (int i = 0; i < plazas.length && !pararCuandoNull; i++) {
-                    pararCuandoNull=true;
+    public void aparcarVehiculo(Vehiculo vehiculoNuevo) { //pasamos por parámetros el nuevo objeto vehículo creado
+        if (vehiculoNuevo.getSize() == 1) { //Este if es para los coches
+            boolean pararCuandoNull = false; //Este booleano es necesario para evitar que, tras retirar un vehículo y deje de seguirse el orden del índice, se aparque en todas las posiciones null.
+            for (int i = 0; i < plazas.length && !pararCuandoNull; i++) {
+                if (this.plazas[i] == null) {
+                    pararCuandoNull = true;
                     this.plazas[i] = vehiculoNuevo;
-                    this.plazas[i + 1] = vehiculoNuevo;
                     System.out.println("Vehículo aparcado correctamente.");
                 }
             }
         }
-
+        if (vehiculoNuevo.getSize() == 2) { //Este if es para los camiones.
+            boolean pararCuandoNull = false;
+            for (int i = 0; i < plazas.length && !pararCuandoNull; i++) {
+                pararCuandoNull = true;
+                this.plazas[i] = vehiculoNuevo;
+                this.plazas[i + 1] = vehiculoNuevo;
+                System.out.println("Vehículo aparcado correctamente.");
+            }
+        }
     }
-
 
     public void desaparcarVehiculo(String matriculaVehiculo) {
         for (int i = 0; i < this.plazas.length && this.plazas[i] != null; i++) { //Es necesario indicar como condición extra que la plaza no sea null porque si no lanza un error.
@@ -121,6 +92,7 @@ public class Parking {
         return dosLibresConsecutivas;
     }
 
+    //Nos da el número de plaza del vehículo según su matrícula.
     public void obtenerVehiculoPorMatricula(String matricula) {
         for (int i = 0; i < this.plazas.length; i++) {
             if (this.plazas[i] != null && this.plazas[i].getMatricula().equals(matricula)) {
@@ -129,10 +101,14 @@ public class Parking {
         }
     }
 
+    //Nos devuelve los datos del vehículo dada la plaza.
     public void obtenerVehiculoPorPlaza(int plaza) {
         for (int i = 0; i < this.plazas.length; i++) {
             if (i == plaza) {
                 System.out.println("El vehículo aparcado en la plaza " + plaza + " es: \n" + this.plazas[plaza].toString());
+               /* if (this.plazas[plaza].getMatricula() == null) {
+                    System.out.println("No hay ningún vehículo aparcado en esta plaza.");
+                }*/
             }
         }
     }
